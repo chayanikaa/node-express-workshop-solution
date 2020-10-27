97fc753ff8296da4ff6c703a80b0a1ebf1d083f2
jd17md <chayanika.khatua@ing.com>
2020-10-27 21:38:42 +0100

    doc: add readme

--- routes/animals.js
+++ routes/animals.js
@@ -4,4 +4,9 @@ animalsRouter.get('/animals', (req, res, next) => {
   res.json(require('../data/animals.json'));
 });
 
+animalsRouter.post('/animals', (req, res, next) => {
+  console.log(req.body);
+  res.status(200).json({status: 'OK'});
+});
+
 module.exports = animalsRouter;
